eacfbbaf86e93156010524c770a8d5a77d7fbdd5
Jimmy Aldape <jimmy@aldape.dev>
2022-07-25 07:03:51 -0700

    added player class

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 import './src/style.css'
+import { Player } from "./src/js/player.js";
 
 const canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 const context = canvas.getContext('2d');
 
-console.log(context);
+const player = new Player(context);
+
+player.draw();
