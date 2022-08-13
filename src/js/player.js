76fd8a1ae8fcd55e3b68b60fc16f02efaf7a2883
Isabella Aldape <isabella@aldape.dev>
2022-08-13 12:47:38 -0700

    adds velocity to player

--- src/js/player.js
+++ src/js/player.js
@@ -5,7 +5,10 @@ class Player {
             x: 100,
             y: 100
         };
-
+        this.velocity = {
+            x: 0,
+            y: 1,
+        }
         this.width = 30;
         this.height = 30;
     }
@@ -14,6 +17,12 @@ class Player {
         this.context.fillStyle = 'red';
         this.context.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
+
+    update() {
+        this.position.y += this.velocity.y;
+        this.draw();
+    }
+
 }
 
 export { Player }
