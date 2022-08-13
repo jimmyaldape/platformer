d52fa5e741e08a04c645d7385a9f8a8b2be8aa4e
Jimmy Aldape <jimmy@aldape.dev>
2022-08-13 12:54:29 -0700

    adds gravity

--- src/js/player.js
+++ src/js/player.js
@@ -1,18 +1,34 @@
+const gravity = .35;
 class Player {
     constructor(context) {
         this.context = context;
+
         this.position = {
             x: 100,
             y: 100
         };
 
+        this.velocity = {
+            x: 0,
+            y: 1
+        };
+
         this.width = 30;
+
         this.height = 30;
     }
 
     draw() {
         this.context.fillStyle = 'red';
         this.context.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update() {
+    
+        this.draw();
+        this.position.y += this.velocity.y;
+
+        this.velocity.y += gravity;
     }
 }
 
