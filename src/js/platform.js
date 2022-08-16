f27bc22231484847134c3ec37b42c28fc47ab8f5
Jimmy Aldape <jimmy@aldape.dev>
2022-08-16 15:01:34 -0700

    define first platform

--- src/js/platform.js
+++ src/js/platform.js
@@ -2,16 +2,17 @@ class Platform {
     constructor(context) {
         this.context = context;
         this.position = {
-            x: 0,
-            y: 0
+            x: 200,
+            y: 500
         }
 
         this.width = 200;
-        this.height = 200;
+        this.height = 30;
     }
 
     draw() {
-        this.context.fillRect();
+        this.context.fillStyle = 'blue';
+        this.context.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 }
 
